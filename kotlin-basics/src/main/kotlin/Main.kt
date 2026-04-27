03f016bd8173219c3d75f0e19e4eac3726ef8d85
AagoshRajSri <aagoshsrivastava10@gmail.com>
2026-04-27 16:35:34 +0530

    Add Kotlin basics project properly

---
+++ kotlin-basics/src/main/kotlin/Main.kt
@@ -0,0 +1,119 @@
+#!/usr/bin/env kotlin
+
+import com.sun.jdi.IntegerType
+
+//fun main(){
+//    // data types
+//    val v = 5
+//    val w = "Hello World"
+//    val x = 5.2
+//    val y: Double = 5.44343
+//    val z = true
+//    println(v)
+//    println(w)
+//    println(x)
+//    println(y)
+//    println(z)
+//}
+
+//fun main(){
+//    // val vs var -> value is immutable and final/constant, whereas variable is a mutable value
+//    var x = "Hello world"
+//    x = "oh na na na"
+//    println(x)
+//}
+
+//}
+
+//}
+
+//fun main(){
+//    // arithmetic operators
+//    var x = 5
+//    x++
+//    x += 5
+//    val y = 10
+//    println(x * y)
+//    println(x + y)
+//    println(y - x)
+//    println(x.toFloat() / y)
+//    println(x % y)
+//fun main(){
+//    // logical operators
+//    val x = 2
+//    val y = 4
+//    val arebothEven = x % 2 == 0 && y % 2 == 0
+//    println("Is x an even number? $arebothEven")
+
+
+//fun main() {
+//    // input from user
+//    println("Enter a number: ")
+//    val input = readln()
+//    val inputAsInteger = input.toIntOrNull()
+//
+//    if (inputAsInteger != null) {
+//        val y = 5 + inputAsInteger
+//        println("The sum of input: $y")
+//    } else {
+//        println("Invalid input")
+//    }
+//}
+
+//fun main(){
+//    // elvis operator
+//    println("Enter a number: ")
+////    val x = readln().toIntOrNull() ?: 1
+//    val x = readln().toIntOrNull()?.rem(2)?.equals(0)
+////    ?.rem(2) → if not null, does number % 2
+////    ?.equals(0) → checks if remainder is 0 (i.e., even number)
+//    println("Result : $x")
+//}
+
+//fun main(){
+//    // if-else condition
+//    println("enter a number: ")
+//    val input = readln().toIntOrNull()
+//
+//    if(input != null){
+//      val isEven = input % 2 == 0
+//       if(isEven){
+//           println("The number is even")
+//       }else{
+//        println("The number is odd")
+//       }
+//    } else{
+//        println("The number is not even")
+//    }
+//}
+
+//fun main() {
+//    // when condition
+//    println("Enter your number: ")
+//    val input = readln().toIntOrNull()
+//
+//    if (input != null) {
+//        val output = when {
+//            input % 2 == 0 -> "The output is even"
+//            input < 10 -> "The output is less than 10 and odd"
+//            else -> "The output is odd and >= 10"
+//        }
+//        println(output)
+//    } else {
+//        println("Enter an integer")
+//    }
+//}
+
+// shorter version
+fun main(){
+    println("Enter ur number: ")
+    val input = readln().toIntOrNull()
+
+    val output = when (input) {
+        null -> "enter a valid integer"
+        2 -> "thats an even number"
+        3 -> "thats an odd number"
+        else -> "i have no idea what to print"
+    }
+    println(output)
+}
